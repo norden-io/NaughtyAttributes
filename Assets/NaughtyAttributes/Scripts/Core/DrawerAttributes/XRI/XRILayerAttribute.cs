dcc9087c6a3706d2c48c8fa4056911dc3aab1c2a
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2023-02-08 19:55:20 +0100

    XRILayer for Unity.XR.Interaction.Toolkit

---
+++ Assets/NaughtyAttributes/Scripts/Core/DrawerAttributes/XRI/XRILayerAttribute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.XR.Interaction.Toolkit;
+using static System.Linq.Enumerable;
+
+#nullable enable
+namespace NaughtyAttributes.XRI
+{
+    public class XRILayerAttribute : BaseLayerAttribute
+    {
+        public override string[] GetLayers()
+        {
+            return Range(0, 32)
+                .Select(i => InteractionLayerMask.LayerToName(i))
+                .Where(name => !string.IsNullOrEmpty(name))
+                .ToArray();
+        }
+
+        public override string LayerToName(int layer)
+        {
+            return InteractionLayerMask.LayerToName(layer);
+        }
+
+        public override int NameToLayer(string layerName)
+        {
+            return InteractionLayerMask.NameToLayer(layerName);
+        }
+    }
+}
